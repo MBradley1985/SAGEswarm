a6ab00e4bb74fc9adb82e27d01868d0d25b356c3
MBradley1985 <2496844@student.swin.edu.au>
2025-12-01 13:39:24 +1100

    -

--- run_pso.sh
+++ run_pso.sh
@@ -5,22 +5,22 @@
 # If any required sage_*.csv files are missing in ./data/, they will be automatically generated from the .hdf5 output after SAGE runs.
 # The output directory is parsed from the OutputDir line in the .par file.
 
-CONFIG_PATH="../../SAGE-2.0/sage-model/input/millennium.par"
-BASE_PATH="../../SAGE-2.0/sage-model/sage"
-OUTPUT_PATH="../../SAGE-2.0/sage-model/output/millennium_pso"
+CONFIG_PATH="../SAGE26/input/millennium.par"
+BASE_PATH="../SAGE26/sage"
+OUTPUT_PATH="../SAGE26/output/millennium_pso"
 PARTICLES=16
 ITERATIONS=10
 TEST="chi2"
 CONSTRAINTS="SMF_z0,BHMF_z0,BHBM,CSFRDH"
-AGE_ALIST_FILE_MINI_UCHUU='/fred/oz004/msinha/simulations/uchuu_suite/miniuchuu/mergertrees/u400_planck2016_50.a_list'
-AGE_ALIST_FILE_MINI_MILLENNIUM="../../SAGE-2.0/sage-model/input/millennium/trees/millennium.a_list"
+AGE_ALIST_FILE_MINI_UCHUU='../simulations/uchuu_suite/miniuchuu/mergertrees/u400_planck2016_50.a_list'
+AGE_ALIST_FILE_MINI_MILLENNIUM="../SAGE26/input/millennium/trees/millennium.a_list"
 BOXSIZE=62.5
 SIM_MINI_UCHUU=0
 SIM_MINI_MILLENNIUM=1
 VOL_FRAC=1.0
 OMEGA0=0.25 
 H0=0.73
-CSVOUTPUT="../../SAGE-2.0/sage-model/output/millennium_pso/params_z0.csv"
+CSVOUTPUT="../SAGE26/output/millennium_pso/params_z0.csv"
 SPACEFILE="./space.txt"
 ACCOUNT="oz004"
 
